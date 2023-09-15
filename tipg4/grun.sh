50a6d2f88c10b902580edecef261fe546523ee8c
yna2jd <123516015+yna2jd@users.noreply.github.com>
2023-09-15 19:09:39 -0400

    Updated both scripts and readme to present version of antlr (Low impact)

--- tipg4/grun.sh
+++ tipg4/grun.sh
@@ -1,1 +1,1 @@
-java -cp .antlr:../build/include/antlr-4.10.1-complete.jar org.antlr.v4.gui.TestRig TIP program 
+java -cp .antlr:../build/include/antlr-4.13.0-complete.jar org.antlr.v4.gui.TestRig TIP program
