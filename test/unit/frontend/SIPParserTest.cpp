230880910f2b7d7b2f8b007d9962ad0b61a555fd
yna2jd <123516015+yna2jd@users.noreply.github.com>
2023-09-15 23:56:19 -0400

    Added stream modifier checks, increment/decrement tests, and started boolean tests

--- test/unit/frontend/SIPParserTest.cpp
+++ test/unit/frontend/SIPParserTest.cpp
@@ -5,6 +5,8 @@
 #include <catch2/catch_test_macros.hpp>
 #include <sstream> //basically just to get JetBrains to be quiet
 
+//Conditionals
+
 TEST_CASE("SIP Parser: conditional >=", "[SIP Parser]") {
     std::stringstream stream;
     stream << R"(
@@ -17,7 +19,6 @@ TEST_CASE("SIP Parser: conditional >=", "[SIP Parser]") {
     )";
     REQUIRE(ParserHelper::is_parsable(stream));
 }
-
 TEST_CASE("SIP Parser: <= conditional", "[SIP Parser]") {
     std::stringstream stream;
     stream << R"(
@@ -33,7 +34,6 @@ TEST_CASE("SIP Parser: <= conditional", "[SIP Parser]") {
 
     REQUIRE(ParserHelper::is_parsable(stream));
 }
-
 TEST_CASE("SIP Parser: < conditional", "[SIP Parser]"){
     std::stringstream stream;
     stream << R"(
@@ -48,4 +48,112 @@ TEST_CASE("SIP Parser: < conditional", "[SIP Parser]"){
     )";
     REQUIRE(ParserHelper::is_parsable(stream));
 }
+TEST_CASE("SIP Parser: insertion stream modifiers are not supported", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+        var x;
+        x << in;
+        return in;
+        }
+    )";
+    REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+TEST_CASE("SIP Parser: Space separated conditional", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+            var x;
+            x = 8;
+            if(x < = 10){
+                x = 0;
+            }
+            return in;
+        }
+    )";
+    REQUIRE_THROWS_MATCHES(FrontEnd::parse(stream), ParseError,
+                           ContainsWhat("extraneous input '='"));
+}
+TEST_CASE("SIP Parser: extraction stream modifiers are not supported", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+        var x;
+        x >> in;
+        return in;
+        }
+    )";
+    REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
 
+//Increment/Decrement
+
+TEST_CASE("SIP Parser: increment", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+            var x;
+            x = 8;
+            x++;
+            return in;
+        }
+    )";
+    REQUIRE(ParserHelper::is_parsable(stream));
+}
+TEST_CASE("SIP Parser: Incorrectly written increment", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+            in+ +;
+            return in;
+        }
+    )";
+    REQUIRE_THROWS_MATCHES(FrontEnd::parse(stream), ParseError,
+                           ContainsWhat("no viable alternative at input"));
+}
+TEST_CASE("SIP Parser: decrement", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+            var x;
+            x = 8;
+            x--;
+            return in;
+        }
+    )";
+    REQUIRE(ParserHelper::is_parsable(stream));
+}
+TEST_CASE("SIP Parser: Incorrectly written decrement", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+        conditional(in){
+            in- -;
+            return in;
+        }
+    )";
+    REQUIRE_FALSE(ParserHelper::is_parsable(stream));
+}
+
+//Boolean type
+TEST_CASE("SIP Parser: Boolean case", "[SIP Parser]"){
+    std::stringstream stream;
+    stream << R"(
+    test(in){
+        var out;
+        if(true){
+            while(in == false){
+                if(in > 0){
+                    in = true;
+                }else{
+                    in = in + 1;
+                }
+            }
+        }else{
+            out = not true;
+        }
+    out = true;
+    return out;
+    }
+    )";
+    REQUIRE(ParserHelper::is_parsable(stream));
+}
