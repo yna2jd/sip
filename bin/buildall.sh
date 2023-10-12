fe013b46a601ffc4d806d5212df263bf6d241cdd
yna2jd <123516015+yna2jd@users.noreply.github.com>
2023-10-12 15:52:17 -0400

    Add coverage cleaning to old scripts

--- bin/buildall.sh
+++ bin/buildall.sh
@@ -1,5 +1,6 @@
 declare -r ROOT_DIR=${TIPDIR:-$(git rev-parse --show-toplevel)}
 cd ${ROOT_DIR}/build/
+../bin/cleancov.sh
 cmake .. >  ${ROOT_DIR}/build/.buildall
 echo cmake done
 make -s -j7 all > ${ROOT_DIR}/build/.buildall
