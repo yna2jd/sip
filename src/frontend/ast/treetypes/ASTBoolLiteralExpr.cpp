a3279b90c78ac22ff33045e11a2468df44d3fbcc
cmiller8677 <102763644+cmiller8677@users.noreply.github.com>
2023-10-12 15:58:51 -0400

    Create ASTBoolLiteralExpr.cpp

---
+++ src/frontend/ast/treetypes/ASTBoolLiteralExpr.cpp
@@ -0,0 +1,12 @@
+#include "ASTBoolLiteralExpr.h"
+#include "ASTVisitor.h"
+
+void ASTBoolLiteralExpr::accept(ASTVisitor *visitor) {
+  visitor->visit(this);
+  visitor->endVisit(this);
+}
+
+std::ostream &ASTBoolLiteralExpr::print(std::ostream &out) const {
+  out << getValue();
+  return out;
+} 
