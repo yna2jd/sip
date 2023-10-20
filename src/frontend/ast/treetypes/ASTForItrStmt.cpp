db2ccce83620b292a660c15924e9df4abf2c5a95
cmiller8677 <102763644+cmiller8677@users.noreply.github.com>
2023-10-20 13:03:06 -0400

    Create ASTForItrStmt.cpp

---
+++ src/frontend/ast/treetypes/ASTForItrStmt.cpp
@@ -0,0 +1,24 @@
+#include "ASTForItrStmt.h"
+#include "ASTVisitor.h"
+
+void ASTForItrStmt::accept(ASTVisitor *visitor) {
+  if (visitor->visit(this)) {
+    getLeft()->accept(visitor);
+    getRight()->accept(visitor);
+    getBody()->accept(visitor);
+  }
+  visitor->endVisit(this);
+}
+
+std::ostream &ASTForItrStmt::print(std::ostream &out) const {
+  out << "for (" << *getLeft() << " : " << *getRight() << ") " << *getBody();
+  return out;
+}
+
+std::vector<std::shared_ptr<ASTNode>> ASTForItrStmt::getChildren() {
+  std::vector<std::shared_ptr<ASTNode>> children;
+  children.push_back(LEFT);
+  children.push_back(RIGHT);
+  children.push_back(BODY);
+  return children;
+}
