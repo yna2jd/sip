a6f777ec170becc530f0c6fb1e07bf7ea5c56643
cmiller8677 <102763644+cmiller8677@users.noreply.github.com>
2023-11-19 23:27:46 -0500

    Update CodeGenFunctions.cpp ret val

--- src/codegen/CodeGenFunctions.cpp
+++ src/codegen/CodeGenFunctions.cpp
@@ -1178,9 +1178,11 @@ llvm::Value *ASTArrayIndexExpr::codegen() {
    LOG_S(1) << "createload " << *this;
    auto val = Builder.CreateLoad(IntegerType::getInt64Ty(TheContext), gep);
    LOG_S(1) << "ret pointer " << *this;
-   return Builder.CreatePtrToInt(val, Type::getInt64Ty(TheContext),
+   return val;
+  //return Builder.CreatePtrToInt(val, Type::getInt64Ty(TheContext),
                                 "arrayAccess");
    // return Builder.CreateLoad(IntegerType::getInt64Ty(TheContext), gep);
+
 }
 
 llvm::Value *ASTNegationExpr::codegen() {
